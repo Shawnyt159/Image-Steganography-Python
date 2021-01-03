02c1a6b30715ca584c43351bbf2a4acef968ae4f
Shawn Trznadel <Strznadel1@gmail.com>
2021-01-02 22:18:36 -0500

    Update

--- Encrypt_Steganography/Encrypt.py
+++ Encrypt_Steganography/Encrypt.py
@@ -1,8 +1,5 @@
-import os
+import sys
 from PIL import Image
-import cv2
-import glob
-from subprocess import call
 
 
 # Convert encoding data into 8-bit binary
@@ -105,15 +102,9 @@ def encode_message_two_input_cipher(message):
     return encoded_message
 
 
-def encode_lsb_image():
-    image = None
-    img_file_name = ''
-    while img_file_name == '':
-        img_file_name = input("Enter image with extension: ")
-        if img_file_name == '':
-            print("Please enter a valid response: ")
+def encode_lsb_image(image_file_name, new_image_file):
     try:
-        image = Image.open(img_file_name, "r")
+        image = Image.open(image_file_name, "r")
     except FileNotFoundError:
         print("Image not found")
         exit(2)
@@ -128,12 +119,7 @@ def encode_lsb_image():
     new_image = image.copy()
 
     encode_message_image(new_image, message)
-    new_image_name = ''
-    while new_image_name == '' or not"." in new_image_name:
-        new_image_name = input("Enter the name for the encoded image with png extension: ")
-        if new_image_name == '' or not".png" in new_image_name:
-            print("You haven't entered a new image name that is compatible: ")
-    new_image.save(new_image_name, str(new_image_name.split(".")[1].upper()))
+    new_image.save(new_image_file, str(new_image_file.split(".")[1].upper()))
     print("Process complete.")
 
 
@@ -190,138 +176,12 @@ def decode_lsb_image():
         pixels = pixel_iterator.__next__()[:3]
 
 
-def delete_video_frames_directory(path):
-    for file_name in glob.glob(path+'/*'):
-        try:
-            os.remove(file_name)
-        except OSError as e:
-            print("Error: %s : %s" % (file_name, e.strerror))
-    os.rmdir(path)
-
-
-def save_frames_as_video(path, new_video_name):
-    video_frames_directory = "video_frames/video_frame%d.jpg"
-    call(["ffmpeg", "-i", video_frames_directory, "-vcodec", "jpeg2000", new_video_name + ".avi", "-y"])
-    delete_video_frames_directory(path)
-
-
-def extract_frames_from_video_and_encode_message(video, message, new_video_name):
-    directory = "video_frames"
-    parent_directory = os.getcwd() + "/"
-    path = os.path.join(parent_directory, directory)
-    os.mkdir(path)
-
-    frame_number = 0
-    while video.isOpened():
-        retrieved_frame, frame = video.read()
-        if not retrieved_frame:
-            break
-        cv2.imwrite(os.path.join(path, 'video_frame'+str(frame_number)+'.jpg'), frame)
-        frame_number += 1
-
-    video.release()
-    cv2.destroyAllWindows()
-    file = path+'/video_frame0.jpg'
-    first_frame = Image.open(file, "r")
-
-    # Encoding the message as cipher text.
-    message = encode_message_two_input_cipher(message)
-    # Encoding the message in the first frame.
-    encode_message_image(first_frame, message)
-    first_frame.save(file)
-    # Saving the frames as a video.
-    save_frames_as_video(path, new_video_name)
-
-
-def encode_lsb_video():
-    video_name = input("Enter the video with extension: ")
-    video = cv2.VideoCapture(video_name)
-    message = input("Enter the message you wish to encrypt: ")
-    new_video_name = input("Enter the name of the new video you wish without extension: ")
-    extract_frames_from_video_and_encode_message(video, message, new_video_name)
-
-
-def decode_lsb_image_no_input(img_file_name):
-    image = Image.open(img_file_name, "r")
-
-    encoded_message = ''
-    image_data = image.getdata()
-    pixel_iterator = iter(image_data)
-
-    while True:
-
-        pixels = [value for value in pixel_iterator.__next__()[:3] +
-                  pixel_iterator.__next__()[:3] +
-                  pixel_iterator.__next__()[:3]]
-
-        binary_string = ''
-        for color_value in pixels[:8]:
-            if color_value % 2 == 0:
-                binary_string += '0'
-            else:
-                binary_string += '1'
-        character_to_add = chr(int(binary_string, 2))
-        encoded_message += character_to_add
-        if pixels[-1] % 2 != 0:
-            return encoded_message
-
-        pixels = pixel_iterator.__next__()[:3]
-
-
-def decode_lsb_video():
-    video_name = input("Enter the video you wish to decode: ")
-    video = cv2.VideoCapture(video_name)
-    retrieved_frame, frame = video.read()
-    first_frame = 'video_frame0.jpg'
-    if not retrieved_frame:
-        return "No frames found in video"
-    cv2.imwrite(first_frame, frame)
-    message = decode_lsb_image_no_input(first_frame)
-    print(message)
-
-
 def main():
-    print("Welcome to steganography, pick your option: ")
-    encode_or_decode = input("Enter the number that corresponds:\n"
-                             "1.) Encode\n"
-                             "2.) Decode\n"
-                             "3.) Exit\n"
-                             ": ")
-    if encode_or_decode == '1':
-        print("Pick your method: ")
-        encode_method = input("Enter the number that corresponds:\n"
-                              "1.) Least Significant Bit image\n"
-                              "2.) Least Significant Bit video\n"
-                              "3.) Exit\n"
-                              ": ")
-        if encode_method == '1':
-            encode_lsb_image()
-            print('Success.')
-        elif encode_method == '2':
-            encode_lsb_video()
-            print('Success.')
-        else:
-            print("Thanks for using the program!\n")
-            exit(1)
-    elif encode_or_decode == '2':
-        print("Pick your method: ")
-        decode_method = input("Enter the number that corresponds:\n"
-                              "1.) Least Significant Bit image\n"
-                              "2.) Least Significant Bit video\n"
-                              "3.) Exit\n"
-                              ": ")
-        if decode_method == '1':
-            encoded_message = decode_lsb_image()
-            readable_message = decode_message_two_input_cipher(encoded_message)
-            print(readable_message)
-        elif decode_method == '2':
-            decode_lsb_video()
-        else:
-            print("Thanks for using the program!\n")
-            exit(1)
+
+    if len(sys.argv) == 5 and sys.argv[1] == '-i' and sys.argv[3] == '-o':
+        encode_lsb_image(sys.argv[2], sys.argv[4])
     else:
-        print("Thanks for using the program!\n")
-        exit(1)
+        print('Incorrect argument format: -i input_file -o output_file')
 
 
 if __name__ == '__main__':
